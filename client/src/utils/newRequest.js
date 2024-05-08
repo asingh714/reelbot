87c66b9e3ec79174ec2f7587e0487fad63bd8137
Aman <amsingh714@gmail.com>
2024-05-08 11:02:07 -0400

    changed to appropriate links

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -3,8 +3,8 @@ import axios from "axios";
 // This instance will be used for all HTTP requests in the app
 // to maintain consistency and reusability
 const newRequest = axios.create({
-  // baseURL: "https://reelbot-server.onrender.com",
-  baseURL: "http://localhost:3001",
+  baseURL: "https://reelbot-server.onrender.com",
+  // baseURL: "http://localhost:3001",
   withCredentials: true,
 });
 
